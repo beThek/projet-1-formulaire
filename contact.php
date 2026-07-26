5cd342b1698bda15d6dd553512160c19bc3454b3
beTheK <26960801+beThek@users.noreply.github.com>
2026-07-26 16:32:30 +0000

    chore: remove personal details from public demo pages

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <info@bethek.5gbfree.com>';
+$from = 'Demo contact form <noreply@example.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <kreshnikiberdemaj@gmail.com>';
+$sendTo = 'Demo contact form <recipient@example.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
